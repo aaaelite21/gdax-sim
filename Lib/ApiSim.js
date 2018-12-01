04e61055c5bb05f71f7160becd9035967098bedc
alexander ali <alexander_ali@my.uri.edu>
2018-12-01 17:48:56 -0500

    added limit order placement and cancleation

--- Lib/ApiSim.js
+++ Lib/ApiSim.js
@@ -1,21 +1,93 @@
 const WebocketSim = require('./WebsocketSim');
-
+const crypto = require('crypto');
 class ApiSim {
     constructor(candles) {
-        this.userLimitOrders = [];
+        this.userLimitOrders = {
+            sells: [],
+            buys: []
+        };
+        this.currentPrice = 0;
         //this.WebsocketClient = WebsocketSim;
     }
 
-
-
     buy(buyParams, callback) {
-
+        buyParams.side = "buy";
+        this.createOrder(buyParams, callback);
     }
     sell(sellParams, callback) {
-
+        sellParams.side = "sell";
+        this.createOrder(sellParams, callback);
     }
     cancelOrder(orderId, callback) {
+        let data;
+        let buyIndex = this.userLimitOrders.buys.map((e) => {
+            return e.id;
+        }).indexOf(orderId);
 
+        let sellIndex = this.userLimitOrders.sells.map((e) => {
+            return e.id;
+        }).indexOf(orderId);
+
+        if (buyIndex === -1 && sellIndex === -1) {
+            data = {
+                message: 'no order by that id'
+            };
+        } else if (buyIndex !== -1) {
+            data = this.userLimitOrders.buys.splice(buyIndex, 1)[0].id;
+        } else if (sellIndex !== -1) {
+            data = this.userLimitOrders.sells.splice(sellIndex, 1)[0].id;
+        }
+        console.log(data)
+        if (typeof callback === 'function') {
+            callback(null, null, data);
+        }
+    }
+
+    createOrder(orderParams, callback) {
+        let data;
+        let order = {}
+        let orderPrice = parseFloat(orderParams.price);
+
+        if ((orderParams.side === 'buy' && orderPrice > this.currentPrice) ||
+            (orderParams.side === 'sell' && orderPrice < this.currentPrice)) {
+            data = {
+                status: 'rejected'
+            }
+
+        } else {
+            //user defined perams
+            order.price = orderParams.price.toString();
+            order.post_only = false;
+            order.product_id = orderParams.product_id;
+            order.size = orderParams.size.toString();
+            order.type = "limit";
+            order.stp = "dc";
+
+
+            //Gdax defined peramns
+            order.id = crypto.createHash('sha1').update(JSON.stringify(orderParams)).digest("hex");
+            order.side = orderParams.side;
+            order.time_in_force = "GTC";
+            order.filled_size = "0.00000000";
+            order.executed_value = "0.0000000000000000";
+            order.status = "pending";
+            order.settled = false;
+            order.fill_fees = "0.0000000000000000";
+            order.created_at = "2016-12-08T20:02:28.53864Z";
+            //save order
+            if (orderParams.side === "buy") {
+                this.userLimitOrders.buys.push(order);
+            } else {
+                this.userLimitOrders.sells.push(order);
+            }
+            //set data to order for callback
+            data = order;
+        }
+
+
+        if (typeof callback === 'function') {
+            callback(null, null, data)
+        }
     }
 
 
@@ -37,3 +109,5 @@ class ApiSim {
         //   }
     }
 }
+
+module.exports = ApiSim;
--- test/ApiSimTest.js
+++ test/ApiSimTest.js
@@ -1,2 +1,184 @@
 const ApiSim = require('../Lib/ApiSim');
 const assert = require('assert');
+const crypto = require('crypto');
+const buyParams = {
+    price: 25.00,
+    size: 0.1,
+    product_id: 'LTC-USD'
+};
+const sellParams = {
+    price: 38.00,
+    size: 0.1,
+    product_id: 'LTC-USD'
+};
+describe('#ApiSim', () => {
+    describe('#init', () => {
+        it('has an array of user limit orders', () => {
+            let Gdax = new ApiSim();
+            assert.deepEqual(Gdax.userLimitOrders, {
+                sells: [],
+                buys: []
+            });
+        });
+    });
+
+    describe("#cancleOrder", () => {
+        it('runs the callback', (done) => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 35;
+            Gdax.buy(buyParams, (err, res, data) => {
+                Gdax.cancelOrder(data.id, done)
+            });
+        });
+        it('returns the order id in the \'data\' attribute of the callback if successful', () => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 35;
+            let orderId;
+            Gdax.buy(buyParams, (err, res, d) => {
+                orderId = d.id;
+                Gdax.cancelOrder(orderId, (err, res, data) => {
+                    assert.equal(data, orderId);
+                });
+            })
+        });
+        it('removes the order from limit buy order array', () => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 35;
+            Gdax.buy(buyParams, (err, res, d) => {
+                assert.equal(Gdax.userLimitOrders.buys.length, 1);
+                Gdax.cancelOrder(d.id, (err, res, data) => {
+                    assert.equal(Gdax.userLimitOrders.buys.length, 0);
+                });
+            });
+        });
+        it('removes the order from limit sell order array', () => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 35;
+            Gdax.sell(sellParams, (err, res, d) => {
+                assert.equal(Gdax.userLimitOrders.sells.length, 1);
+                Gdax.cancelOrder(d.id, (err, res, data) => {
+                    assert.equal(Gdax.userLimitOrders.sells.length, 0);
+                });
+            });
+        });
+    });
+
+    describe('#buy', () => {
+        it('saves the limit order to the userLimitOrders.buys array ', () => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 35;
+            Gdax.buy(buyParams);
+            assert.deepEqual(Gdax.userLimitOrders.buys[0], {
+                id: crypto.createHash('sha1').update(JSON.stringify(buyParams)).digest("hex"),
+                price: buyParams.price.toString(),
+                size: buyParams.size.toString(),
+                product_id: buyParams.product_id,
+                side: "buy",
+                stp: "dc",
+                type: "limit",
+                time_in_force: "GTC",
+                post_only: false,
+                created_at: "2016-12-08T20:02:28.53864Z",
+                fill_fees: "0.0000000000000000",
+                filled_size: "0.00000000",
+                executed_value: "0.0000000000000000",
+                status: "pending",
+                settled: false
+            });
+        });
+        it('runs the callback after', (done) => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 35;
+            Gdax.buy(buyParams, done);
+        });
+        it('returns the order in the \'data\' attribute of the callback', () => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 35;
+            Gdax.buy(buyParams, function (err, res, data) {
+                assert.deepEqual(data, {
+                    id: crypto.createHash('sha1').update(JSON.stringify(buyParams)).digest("hex"),
+                    price: buyParams.price.toString(),
+                    size: buyParams.size.toString(),
+                    product_id: buyParams.product_id,
+                    side: "buy",
+                    stp: "dc",
+                    type: "limit",
+                    time_in_force: "GTC",
+                    post_only: false,
+                    created_at: "2016-12-08T20:02:28.53864Z",
+                    fill_fees: "0.0000000000000000",
+                    filled_size: "0.00000000",
+                    executed_value: "0.0000000000000000",
+                    status: "pending",
+                    settled: false
+                });
+            });
+        });
+        it('rejects the order if the price is above the current price', () => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 12;
+            Gdax.buy(buyParams, (err, res, data) => {
+                assert.equal(data.status, "rejected");
+            });
+        })
+    });
+    describe('#sell', () => {
+        it('saves the limit order to the userLimitOrders.sells array ', () => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 35;
+            Gdax.sell(sellParams);
+            assert.deepEqual(Gdax.userLimitOrders.sells[0], {
+                id: crypto.createHash('sha1').update(JSON.stringify(sellParams)).digest("hex"),
+                price: sellParams.price.toString(),
+                size: sellParams.size.toString(),
+                product_id: sellParams.product_id,
+                side: "sell",
+                stp: "dc",
+                type: "limit",
+                time_in_force: "GTC",
+                post_only: false,
+                created_at: "2016-12-08T20:02:28.53864Z",
+                fill_fees: "0.0000000000000000",
+                filled_size: "0.00000000",
+                executed_value: "0.0000000000000000",
+                status: "pending",
+                settled: false
+            });
+        });
+        it('runs the callback after', (done) => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 35;
+            Gdax.sell(sellParams, done);
+        });
+        it('returns the order in the \'data\' attribute of the callback', () => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 35;
+            Gdax.sell(sellParams, function (err, res, data) {
+                assert.deepEqual(data, {
+                    id: crypto.createHash('sha1').update(JSON.stringify(sellParams)).digest("hex"),
+                    price: sellParams.price.toString(),
+                    size: sellParams.size.toString(),
+                    product_id: sellParams.product_id,
+                    side: "sell",
+                    stp: "dc",
+                    type: "limit",
+                    time_in_force: "GTC",
+                    post_only: false,
+                    created_at: "2016-12-08T20:02:28.53864Z",
+                    fill_fees: "0.0000000000000000",
+                    filled_size: "0.00000000",
+                    executed_value: "0.0000000000000000",
+                    status: "pending",
+                    settled: false
+                });
+            });
+        });
+        it('rejects the order if the price is above the current price', () => {
+            let Gdax = new ApiSim();
+            Gdax.currentPrice = 40;
+            Gdax.sell(sellParams, (err, res, data) => {
+                assert.equal(data.status, "rejected");
+            });
+        })
+    });
+});
