e6c077e74fb8f3ba145ab9ae6c412f05bbf90018
alexander ali <alexander_ali@my.uri.edu>
2019-08-05 21:18:25 -0400

    begun unit tests and requirments

---
+++ test/EventDriverTest.js
@@ -0,0 +1,11 @@
+const ApiSim = require('../gdax-sim');
+const WebsocketSim = require('../Lib/WebsocketSim');
+
+describe('#On Event', () => {
+    describe('#OnBuy', () => {
+
+    });
+    describe('#OnSell', () => {
+
+    });
+});
